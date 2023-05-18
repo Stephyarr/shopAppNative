825106c46292754d2837026fcc24bae031b160b7
Stephyarr <stephaniearratia21@gmail.com>
2023-05-17 23:46:08 -0300

    expo-init

---
+++ src/screens/index.js
@@ -0,0 +1,3 @@
+export { default as Categories } from "./Categories/index";
+export { default as ProductDetail } from "./ProductDetail/index";
+export { default as Products } from "./Products/index";
